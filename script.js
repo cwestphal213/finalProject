1c67e990d269884b5906c4b873c47c9ab010a6a4
cwestphal213 <47224660+cwestphal213@users.noreply.github.com>
2019-05-03 20:18:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+
+var deaths = d3.csv("journalistsKilled.csv")
+var govType = d3.csv("govType.csv")
+var countryPop = d3.csv("countryPop.csv")
+var geoJson = d3.json("custom.geo.json")
+
+
+Promise.all([deaths, govType, countryPop, geoJson]).then(function(values)
+{
+  var deathData = values[0]
+  var govData = values[1]
+  var popData = values[2]
+  var geoJson = values[3]
+
+var countriesDeath = makeCountryData(deathData)
+var countriesGov = makeCountryData(govData)
+var countriesPop = makeCountryData(popData)
+
+
+
+geoJson.features.forEach(function(feature){
+
+  Object.keys(countriesDeath).forEach(function(country){
+    if (feature.properties.name = country){
+    feature.properties.deaths = countriesDeath[country][country]
+    }
+    else{
+      console.log('not found', feature.properties.name)
+    }
+  })
+  })
+
+  geoJson.features.forEach(function(feature){
+
+    Object.keys(countriesGov).forEach(function(country){
+      if (feature.properties.name = country){
+      feature.properties.gov = countriesGov[country][country]
+      }
+      else{
+        console.log('not found', feature.properties.name)
+      }
+    })
+    })
+
+    geoJson.features.forEach(function(feature){
+
+      Object.keys(countriesPop).forEach(function(country){
+        if (feature.properties.name = country){
+        feature.properties.pop = countriesPop[country][country]
+        }
+        else{
+          console.log('not found', feature.properties.name)
+        }
+      })
+      })
+
+      drawMap(geoJson, '1996')
+  })
+
+
+var drawMap = function(geoData, year){
+  var screen = {width:1500,height:1000}
+    //create Projection
+    //var projection = d3.geoAlbersUsa()
+    //                  .translate([screen.width/2,screen.height/2]);
+
+    var geoPath = d3.geoPath()
+    .projection( d3.albersProjection );
+
+
+    var svg = d3.select("svg")
+                .attr("width",screen.width)
+                .attr("height",screen.height);
+
+    var nations = svg.append("g")
+      .selectAll("g")
+      .data(geoData.features)
+      .enter()
+      .append("g")
+
+      nations.append("path")
+      .attr("d",geoPath)
+      .attr("stroke","black")
+      .attr("fill","none");
+
+
+};
+
+
+
+//create a dictionary for the data of every country given the year
+var parseData = function(data, country){
+
+      var dataDict = {}
+      data.forEach(function(state){
+        if (state['Country'] == country){
+          dataDict[country] = state
+        }
+      })
+  return dataDict
+}
+
+var makeCountryData = function(data){
+      var countriesData = {}
+      data.forEach(function(state){
+          countriesData[state["Country"]] = parseData(data, state['Country'])
+      })
+      return countriesData
+}
