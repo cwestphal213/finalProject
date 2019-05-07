2b9b3667bc5cc1909e6c11c3c166fd0ac9029d9b
cwestphal213 <47224660+cwestphal213@users.noreply.github.com>
2019-05-06 23:32:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ geoData.features.forEach(function(feature){
 }
 
 
+}
 
 var drawMap = function(geoData, year){
   var screen = {width:1500,height:1000}
@@ -66,6 +67,7 @@ var drawMap = function(geoData, year){
     //                  .translate([screen.width/2,screen.height/2]);
 
 
+}
 var proj = d3.geoConicEqualArea().translate([screen.width/2, screen.height/2]).scale([250])
 
     var geoPath = d3.geoPath()
@@ -82,24 +84,126 @@ var proj = d3.geoConicEqualArea().translate([screen.width/2, screen.height/2]).s
                   .enter()
                   .append("g")
 //made it
-console.log(geoData.features)
+var reds = ['RGB(255, 236, 25)','RGB(255, 193, 0)','RGB(255, 152, 0)','RGB(255, 86, 7)','RGB(246, 65, 45)']
+var govColors = ['crimson', 'orange', 'darkturquoise', 'dodgerblue']
+
+var govColor = d3.scaleOrdinal()
+                  .domain(['Authoritarian regimes','Hybrid Regime','Flawed Democracy', 'Democracy'])
+                  .range(govColors)
+
+  var popColor = d3.scaleQuantize()
+                .domain([Math.log(9256), Math.log(1386395000)])
+                .range(reds);
+
+var deathPopColor = d3.scaleQuantize()
+                      .domain([0, 4.5])
+                      .range(reds);
+
+                  /*d3.max(geoData, function(d){
+
+                    if (d.properties.pop){
+                      if (d.properties.pop[year]){
+                        console.log('in color',d.properties.pop[year])
+                      return d.properties.pop[year]}
+                    }
+
+                })])*/
+        /*        var tooltip = d3.select("body")
+                  .selectAll('div')
+                  .data(geoData.features)
+                  .enter()
+                	.append("div")
+                	.style("position", "absolute")
+                	.style("z-index", "10")
+                	.style("visibility", "hidden")
+                	.text(function(d){
+                    return d.properties.name
+                  });*/
+
+console.log(year, geoData.features)
                   nations.append("path")
                   .attr("d",geoPath)
                   .attr("stroke","black")
                   .attr("fill", function(d){
                     console.log(d.properties.name)
-                    if (d.properties.pop ){
+                    if (d.properties.pop && d.properties.deaths ){
 
-                    var value = d.properties.pop[year]
+                    var value = d.properties.deaths[year]/ Math.log(d.properties.pop[year])
                     if(value){
-                      console.log('pop', value,  d.properties.name)
-                      return color(value)
+                      max = d3.max(geoData.features, function(d){
+                        if (d.properties.pop && d.properties.deaths){
+                        return d.properties.deaths[year]/ Math.log(d.properties.pop[year])
+                          }
+                      })
+                      console.log('max', max)
+                      return deathPopColor(value)
                     }
                     else {
-                      return "red"
-                    }}
-                  });
+                      return "Gainsboro"
+                    }
 
+                  }
+                    else{
+                      return "DarkGrey"
+                    }
+
+                  })
+                  .on("mouseover", function(d){
+                    var xPosition = parseFloat(d3.select(this).attr('x'));
+                    var yPosition = parseFloat(d3.select(this).attr('y'));
+                    d3.select('#tooltip')
+                      .style('left', xPosition + 'px')
+                      .style('top', yPosition + 'px')
+                    d3.select('#tooltip')
+                      .select('#year')
+                      .text(year)
+                    d3.select('#tooltip')
+                      .select('#country')
+                      .text(d.properties.name)
+                      if ( d.properties.pop){
+                      d3.select('#tooltip')
+                      .select('#pop')
+                      .text( d.properties.pop[year])
+                      if ( d.properties.gov){
+                      d3.select('#tooltip')
+                      .select('#gov')
+                      .text( d.properties.gov[year])
+                    }
+                      if (d.properties.deaths){
+                      d3.select('#tooltip')
+                      .select('#death')
+                      .text(d.properties.deaths[year]);
+                    }
+                    else{
+                      d3.select('#tooltip')
+                      .select('#death')
+                      .text('0');
+                    };
+                      //must get to here to make unhidden
+                      d3.select("#tooltip").classed("hidden", false);
+                    }})
+                      .on("mouseout", function(){
+                        d3.select("#tooltip").classed("hidden", true)
+                      })
+                /*  .append('title')
+                  .text(function(d){
+                    return d.properties.name});*/
+
+        nations.append("circle")
+                    .attr("cx",function(d) {return geoPath.centroid(d)[0]})
+                    .attr("cy",function(d) {return geoPath.centroid(d)[1]})
+                    .attr('r', 3)
+                    .attr('fill', function(d){
+                      if (d.properties.gov && !(d.properties.gov[year] == 'No Data')){
+
+                        return govColor(d.properties.gov[year])
+                      }
+                      else{
+                        return 'black'
+                      }
+                    })
+                    .style('stroke','white');
+                    //add tool tip to this element as well
 
 /*    var nations = svg.append("g")
       .selectAll("g")
@@ -120,8 +224,8 @@ console.log(geoData.features)
         }
       });*/
 };
-var reds = ['red1','red2','red3','red4','red5',]
-var color = d3.scaleQuantize().range(reds)
+
+
 
 
 //create a dictionary for the data of every country given the year
